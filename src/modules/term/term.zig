38fee53fc806165e050102b5a6c61bf643408649
magiruuvelvet <magiruuvelvet@naver.com>
2023-02-05 20:10:19 +0100

    modules/term: implement working version of ESC sequence filter

--- src/modules/term/term.zig
+++ src/modules/term/term.zig
@@ -49,3 +49,56 @@ pub fn wcwidth(str: []const u8) i32 {
 
     return width;
 }
+
+/// get the total required width of the given character sequence
+/// input must be UTF-8 encoded
+///
+/// this function supports ASCII escape sequences in the input string
+///
+/// on success: returns the total required width of the character sequence
+/// on UTF-8 failure: returns -255
+/// on `wcwidth` failure: returns -1
+pub fn wcwidth_ascii(str: []const u8) i32 {
+    return wcwidth(filter_ascii_escape_sequences(str));
+}
+
+/// filters ASCII escape sequences from the given string
+pub fn filter_ascii_escape_sequences(str: []const u8) error{InvalidEscapeSequenceFound,InvalidUtf8,OutOfMemory}![]const u8 {
+    const allocator = std.heap.page_allocator;
+
+    const memory = try allocator.alloc(u8, str.len);
+    //defer allocator.free(memory);
+
+    var i: usize = 0;
+    var pos: usize = 0;
+    var in_sequence: bool = false;
+    while (i < str.len) : (i += 1) {
+        // check if the current character is an ESC character
+        if (str[pos] == "\x1b"[0])
+        {
+            in_sequence = true;
+        }
+
+        // find end of ESC sequence character (m)
+        if (in_sequence)
+        {
+            if (std.mem.indexOf(u8, str[pos..], "m")) |end| {
+                pos += end + 1;
+                in_sequence = false;
+            } else {
+                // found start, but no end [abort and return error]
+                return error.InvalidEscapeSequenceFound;
+            }
+        }
+
+        memory[i] = str[pos];
+        pos += 1;
+
+        if (pos >= str.len)
+        {
+            break;
+        }
+    }
+
+    return memory[0..i+1];
+}
