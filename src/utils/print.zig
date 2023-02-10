02c91d2c42b28cd950550e5885dc27b2a7a9f252
magiruuvelvet <magiruuvelvet@naver.com>
2023-02-10 18:27:13 +0100

    utils/print: add stderr print function

--- src/utils/print.zig
+++ src/utils/print.zig
@@ -1,12 +1,21 @@
 const std = @import("std");
 
 const stdout_file = std.io.getStdOut().writer();
-var bw = std.io.bufferedWriter(stdout_file);
-const stdout = bw.writer();
+var stdout_bw = std.io.bufferedWriter(stdout_file);
+const stdout = stdout_bw.writer();
+
+const stderr_file = std.io.getStdErr().writer();
+var stderr_bw = std.io.bufferedWriter(stderr_file);
+const stderr = stderr_bw.writer();
 
 pub inline fn print(comptime fmt: []const u8, args: anytype) void {
     stdout.print(fmt, args) catch unreachable;
-    bw.flush() catch unreachable;
+    stdout_bw.flush() catch unreachable;
+}
+
+pub inline fn err(comptime fmt: []const u8, args: anytype) void {
+    stderr.print(fmt, args) catch unreachable;
+    stderr_bw.flush() catch unreachable;
 }
 
 /// directy write text to terminal without formatting
@@ -23,5 +32,5 @@ pub inline fn write_no_flush(text: []const u8) void {
 
 /// flush the output stream
 pub inline fn flush() void {
-    bw.flush() catch unreachable;
+    stdout_bw.flush() catch unreachable;
 }
